2ce3258ca6896b964f36d4438d5d2fc1c50d3905
yukun <15152347277@163.com>
2017-06-07 19:35:38 +0800

    启动程序脚本

--- manage.py
+++ manage.py
@@ -1,3 +1,18 @@
-from app import app
+#!/usr/bin/env python
+from app import app, db
+from app.models import User, Role
+from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
 
-app.run(debug=True)
+
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
+
+if __name__ == '__main__':
+    manager.run()
