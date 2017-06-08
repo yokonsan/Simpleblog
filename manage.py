eab0496d6e7650d3a318633e361f8bc00874df5a
yukun <15152347277@163.com>
2017-06-08 22:34:42 +0800

    启动程序添加测试脚本

--- manage.py
+++ manage.py
@@ -13,6 +13,13 @@ def make_shell_context():
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 
 if __name__ == '__main__':
     manager.run()
