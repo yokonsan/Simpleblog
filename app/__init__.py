28f5b80f9da7927d6c75eabee51c906c52c3d2eb
yukun <15152347277@163.com>
2017-06-12 22:19:16 +0800

    将app的bug写入tmp文件

--- app/__init__.py
+++ app/__init__.py
@@ -13,4 +13,16 @@ lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 
+if not app.debug:
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('tmp/microblog.log', 'a',
+                                       1 * 1024 * 1024, 10)
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+app.logger.info('microblog startup')
+
 from app import views, models
