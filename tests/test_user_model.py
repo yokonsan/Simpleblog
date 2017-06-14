2b88736e8f6a5991a803cce3b61c3e88b0662194
yukun <15152347277@163.com>
2017-06-14 22:13:57 +0800

    角色权限测试文件

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,8 +1,22 @@
 import unittest
-from app.models import User
+import os
+from config import basedir
+from app.models import User, Role, Permission, AnonymousUser
+from app import app, db
 
 
 class UserModelTestCase(unittest.TestCase):
+    # def setUp(self):
+    #     self.app = app('testing')
+    #     self.app_context = self.app.app_context()
+    #     self.app_context.push()
+    #     db.create_all()
+    #     Role.insert_roles()
+    #
+    # def tearDown(self):
+    #     db.session.remove()
+    #     db.drop_all()
+    #     self.app_context.pop()
 
     def test_password_setter(self):
         u = User(password='cat')
@@ -22,3 +36,14 @@ class UserModelTestCase(unittest.TestCase):
         u = User(password='cat')
         u2 = User(password='cat')
         self.assertTrue(u.password_hash != u2.password_hash)
+
+    # 测试角色和权限
+    def test_roles_and_permissions(self):
+        Role.insert_roles()
+        u = User(email='john@example.com', password='cat')
+        self.assertTrue(u.operation(Permission.WRITE_ARTICLES))
+        self.assertFalse(u.operation(Permission.MODERATE_COMMENTS))
+
+    def test_anonymous_user(self):
+        u = AnonymousUser()
+        self.assertFalse(u.operation(Permission.FOLLOW))
